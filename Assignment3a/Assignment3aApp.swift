c282775c8e2b12135c0eb3203d689ee3d6518b24
Kevin Earls <kearls@redhat.com>
2022-01-05 17:00:52 +0100

    Initial Commit

---
+++ Assignment3a/Assignment3aApp.swift
@@ -0,0 +1,17 @@
+//
+//  Assignment3aApp.swift
+//  Assignment3a
+//
+//  Created by Kevin Earls on 05/01/2022.
+//
+
+import SwiftUI
+
+@main
+struct Assignment3aApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
